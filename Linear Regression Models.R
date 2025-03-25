7309c3d5ba0ad823138167d0ec8d50a243e797e8
Laney Tellegen <laneytellegen@Karens-MacBook-Pro-2.local>
2025-03-25 10:56:13 -0500

    Creating same models with DIv and CHT variables

--- Linear Regression Models.R
+++ Linear Regression Models.R
@@ -26,6 +26,39 @@ ggplot(rawdata2,aes(x=Day, y=ER, color=RunNumFac))+
   geom_smooth(method = "lm")
 
 
+#SAME THING BUT WITH DIV ---> I DONT THINK THIS IS A APPROPRIATE MODEL FOR THIS
+
+#looking at how progression of number of Diversions changes over DAY, separated by run number
+options(contrasts=c("contr.helmert","contr.poly"))
+DIVmodelfac<-lme(DIV~RunNumFac*Day, correlation=corAR1(form=~Day|Group/RunNumFac), random=~1|Group,data=rawdata2)
+summary(DIVmodelfac)
+anova(DIVmodelfac,type="marginal")
+#LOOK BACK AT THIS!!! Becuase DIV is factorial (1-4)
+
+ggplot(rawdata2,aes(x=Day, y=DIV, color=RunNumFac))+
+  geom_point()+
+  ggtitle("Efficiency of P. polycephalum over Maze Attempts")+
+  labs(x="Day", y="Dead Ends")+
+  geom_smooth(method = "lm")
+
+
+
+#Same thing but with Cht
+
+#looking at how progression of cheating instances changes over DAY, separated by run number
+options(contrasts=c("contr.helmert","contr.poly"))
+CHTmodelfac<-lme(CHT~RunNumFac*Day, correlation=corAR1(form=~Day|Group/RunNumFac), random=~1|Group,data=rawdata2)
+summary(CHTmodelfac)
+anova(CHTmodelfac,type="marginal")
+#Results
+
+
+ggplot(rawdata2,aes(x=Day, y=CHT, color=RunNumFac))+
+  geom_point()+
+  ggtitle("Efficiency of P. polycephalum over Maze Attempts")+
+  labs(x="Day", y="Cheating Instances (wall jumping)")+
+  geom_smooth(method = "lm")
+
 
 
 #General linear effects with randomization for group
@@ -45,7 +78,10 @@ anova(Divmodel)
 
 
 
-#Without Contamination
+
+
+
+#Without Contamination-----
 #getting error "Singularity in backsolve at level 0, block 1'
 
 puredatall<-rawdata2[!rawdata2$CONTAM == "1", ]
